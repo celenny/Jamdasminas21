d7c8052fe4b4d61af56cb3b2313439ecad4561e1
prisaz <pris_az@hotmail.com>
2021-03-14 05:19:50 -0300

    adicionando telas de menu

---
+++ jamdasminas2021/Assets/Script/MenuInicio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChamaInicio()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+
+    public void ChamaJogar()
+    {
+        SceneManager.LoadScene("Jogar");
+    }
+
+    public void ChamaCreditos()
+    {
+        SceneManager.LoadScene("Creditos");
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
+    }
+}
